926208fc3b1b58ac494f6458a780ad859ae5af5e
Leokrut5 <leo13255000@mail.ru>
2025-04-16 22:09:12 +0300

    Добавил стратегию, инженера и модель, пока не тестил

--- src/feature_engineering/strategies.py
+++ src/feature_engineering/strategies.py
@@ -10,7 +10,10 @@ import pandas as pd
 
 from .engineers import (
     TimeSeriesFeatureEngineer,
-    TaxFeatureEngineer
+    TaxFeatureEngineer,
+    MOEXFeatureEngineer,
+    USDRUBFeatureEngineer,
+    InflationFeatureEngineer,
 )
 
 
@@ -39,3 +42,48 @@ class BaselineFeatureEngineerStrategy(BaseFeatureEngineerStrategy):
         out = self.time_series_fe.build_features(df=df, holidays=holidays)
         out = self.tax_fe.build_features(df=out, taxes=taxes)
         return out
+
+
+class ExternalFactorsFeatureEngineerStrategy(BaseFeatureEngineerStrategy):
+    def __init__(
+        self,
+        target: str = 'balance',
+        date_col: str = 'date',
+        lags: Optional[Iterable[int]] = None,
+        moex_lags: Optional[Iterable[int]] = None,
+        usd_lags: Optional[Iterable[int]] = None,
+        inflation_lags: Optional[Iterable[int]] = None,
+    ):
+        # Признаки из временного ряда таргета
+        self.time_series_fe = TimeSeriesFeatureEngineer(target=target, date_col=date_col, lags=lags)
+        self.tax_fe = TaxFeatureEngineer(target=target, date_col=date_col)
+
+        # Внешние признаки
+        self.moex_fe = MOEXFeatureEngineer(date_col=date_col, lags=moex_lags)
+        self.usd_fe = USDRUBFeatureEngineer(date_col=date_col, lags=usd_lags)
+        self.inflation_fe = InflationFeatureEngineer(date_col=date_col, lags=inflation_lags)
+
+    def build_features(
+        self,
+        df: pd.DataFrame,
+        taxes: pd.DataFrame,
+        holidays: Dict[str, Sequence[pd.Timestamp]],
+        moex: pd.DataFrame,
+        usd: pd.DataFrame,
+        inflation: pd.DataFrame,
+    ) -> pd.DataFrame:
+        # Временные признаки на основе таргета
+        out = self.time_series_fe.build_features(df=df, holidays=holidays)
+        out = self.tax_fe.build_features(df=out, taxes=taxes)
+
+        # Внешние признаки
+        moex_feats = self.moex_fe.build_features(moex)
+        usd_feats = self.usd_fe.build_features(usd)
+        inflation_feats = self.inflation_fe.build_features(inflation)
+
+        # Объединение всех фичей
+        out = out.merge(moex_feats, on='date', how='left')
+        out = out.merge(usd_feats, on='date', how='left')
+        out = out.merge(inflation_feats, on='date', how='left')
+
+        return out.dropna()
