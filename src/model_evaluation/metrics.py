87e9fd0dcfda2fa965ef160b026a4419e2273b45
Vsevolod Shurygin <79019835+UralTime@users.noreply.github.com>
2025-04-18 19:56:41 +0300

    not const ruonia and max_ae=0.42

--- src/model_evaluation/metrics.py
+++ src/model_evaluation/metrics.py
@@ -6,13 +6,6 @@ from typing import Sequence
 
 import numpy as np
 
-
-KEY_RATE = 7
-ADDITIONAL_RATE = KEY_RATE + 0.5
-SURPLUS_RATE = KEY_RATE - 0.9
-DEFICIT_RATE = KEY_RATE + 1
-N_DAYS_IN_YEAR = 1 # for better visibility instead of 365
-
 def target_loss(y_true: Sequence, y_pred: Sequence) -> float:
     """Целевая ошибка
 
@@ -28,13 +21,32 @@ def target_loss(y_true: Sequence, y_pred: Sequence) -> float:
     float
         _description_
     """
+    rate_filepath = 'data/rounia.xlsx'
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        rate_df = pd.read_excel(rate_filepath, engine="openpyxl")
+    rate_df['date'] = pd.to_datetime(rate_df['DT'])
+    rate_df = rate_df[['date', 'ruo']].rename(columns={'ruo': 'rate'})
+    rate_df = rate_df.sort_values('date').set_index('date')
+    rate = np.array(rate_df.iloc[idx]['rate'])
+
     y_true = np.array(y_true)
     y_pred = np.array(y_pred)
     diff = y_pred - y_true
-    loss = np.abs(diff) * (
-        (diff > 0) * DEFICIT_RATE
-        + (diff < 0) * (ADDITIONAL_RATE - SURPLUS_RATE)
-    ) / 100 / N_DAYS_IN_YEAR
+    positive_diff = diff.clip(min=0)
+    negative_diff = diff.clip(max=0)
+
+    # экономические потери
+    loss = positive_diff * (rate + 1) / 100 - negative_diff * (rate + 0.5 - (rate - 0.9)) / 100
+
+    # требование заказчика
+    MAX_AE = 0.42
+    MAX_AE_IMPORTANCE = 1e6 # допустим, за ошибку прогноза больше 0.42 по абсолютному значению доп.штраф 1млн
+    excess_level = abs(diff) - MAX_AE
+    excess_level[excess_level > 0] = MAX_AE_IMPORTANCE
+    
+    loss += excess_level
+
     return np.mean(loss)
 
 def maximum_absolute_error(y_true: Sequence, y_pred: Sequence) -> float:
