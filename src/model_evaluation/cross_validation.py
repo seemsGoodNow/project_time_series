80704de332a2aa9097793f581b6b07bfd202b300
Vsevolod Shurygin <79019835+UralTime@users.noreply.github.com>
2025-04-20 10:48:14 +0300

    Update cross_validation.py

--- src/model_evaluation/cross_validation.py
+++ src/model_evaluation/cross_validation.py
@@ -1,105 +1,110 @@
 """
-Модуль содержит подходы для подбора гиперпараметров модели
+Модуль содержит функции для разбиения временного промежутка на train и test
+и проведения кросс-валидации
 """
 
-
-from typing import Optional, Dict, NoReturn, List, Tuple
-from abc import ABC, abstractmethod
+import random
+from dataclasses import dataclass
+from typing import List, Dict, Callable, Tuple
 
 import pandas as pd
-import optuna
-from catboost import CatBoostRegressor
+import numpy as np
 
-from .model_evaluation.metrics import SimpleTargetLoss, MAE, MaxAE
-
-from .model_evaluation.cross_validation import (
-    CrossValidationResult,
-    perform_cross_val,
-)
+from .metrics import Metric, SimpleTargetLoss, MaxAE, MAE
 
 
-class BaseHyperparamsOptimizer(ABC):
+@dataclass(frozen=True, kw_only=True)
+class CrossValidationResult:
+    metrics: Dict[str, List[float]] # key is metric name, value - metric value in fold
+    models: List # list of trained models during cross-validation
+    mean_metrics: Dict[str, float] # key is metric name, value - mean metric value
+
+    def __repr__(self):
+        s = 'CrossValidationResult('
+        for key, val in self.__dict__.items():
+            s += f'\n   {key} = {val}'
+        s += '\n)'
+        return s
     
-    @abstractmethod
-    def get_optimized_params(self):
-        pass
-
-
-class BaselineHyperparamsOptimizer(BaseHyperparamsOptimizer):
-    def __init__(
-        self,
-        splits: List[Tuple[List[int], List[int]]],
-        parameter_ranges: Optional[Dict] = None,
-        cross_val_score_kws: Optional[Dict] = None,
-        optuna_n_trials: int = 20,
-        model_class = CatBoostRegressor
-    ):
-        self.splits = splits
-        self.__define_parameter_ranges(parameter_ranges)
-        self.__define_cross_val_score_kws(cross_val_score_kws)
+def perform_cross_val(
+    model_class,
+    model_params: dict,
+    splits: list,
+    loss: Metric = SimpleTargetLoss(),
+    additional_metrics: Dict[str, Metric] = {'max_ae': MaxAE(), 'mae': MAE()},
+    X=None,
+    y=None,
+) -> CrossValidationResult:
+    models = []
+    if additional_metrics is None:
+        additional_metrics = {}
+    cv_loss = {
+        key: [] for key in additional_metrics
+    }
+    cv_loss['target_loss'] = []
+    for split in splits:
+        if X is None or y is None:
+            X_train, y_train = split[0]
+            X_test, y_test = split[1]
+        else:
+            train_idx, test_idx = split
+            X_train, X_test = X.iloc[train_idx], X.iloc[test_idx]
+            y_train, y_test = y.iloc[train_idx], y.iloc[test_idx]
+        model = model_class(**model_params)
+        model.fit(X_train, y_train)
+        models.append(model)
+        y_pred = model.predict(X_test)
+        for key, metric in additional_metrics.items():
+            cv_loss[key].append(metric.calc(y_test, y_pred))
+        cv_loss['target_loss'].append(loss.calc(y_test, y_pred))
         
-        self.static_params = {
-            key: val for key, val in self.parameter_ranges.items() if not callable(val)
-        }
-        self.optuna_n_trials = optuna_n_trials
-        self.model_class = model_class
-    
-    def get_optimized_params(self, X: pd.DataFrame, y: pd.Series) -> Dict:
-        # Defining objective for optuna search
-        def objective(trial: optuna.trial.Trial) -> float:
-            params = self._get_evaluated_trial_params(trial) # create a dict from passed callables
-            loss = perform_cross_val(
-                model_class=self.model_class,
-                model_params=params,
-                splits=self.splits,
-                X=X, y=y,
-                **self.cross_val_score_kws
-            ).mean_metrics['target_loss']
-            return loss
-        study = optuna.create_study(direction='minimize')
-        study.optimize(objective, n_trials=self.optuna_n_trials)
-        return study.best_params
-    
-    def get_fitted_models_and_metrics(
-        self, 
-        X: pd.DataFrame, 
-        y: pd.Series, 
-        params: Dict
-    ) -> CrossValidationResult:
-        cross_val_result: CrossValidationResult = perform_cross_val(
-            model_class=self.model_class,
-            model_params=params,
-            splits=self.splits,
-            X=X, y=y,
-            **self.cross_val_score_kws
-        )
-        return cross_val_result
-    
-    def _get_evaluated_trial_params(self, trial: optuna.trial.Trial):
-        return {
-            key: val(trial) if callable(val) else val 
-            for key, val in self.parameter_ranges.items()
-        }
-    
-    def __define_parameter_ranges(self, parameter_ranges: Optional[Dict] = None) -> NoReturn:
-        self.parameter_ranges = {
-            'iterations': lambda trial: trial.suggest_int('iterations', 100, 2000),
-            'learning_rate': lambda trial: trial.suggest_float('learning_rate', 0.001, 0.3, log=True),
-            'depth': lambda trial: trial.suggest_int('depth', 2, 7),
-            'l2_leaf_reg': lambda trial: trial.suggest_float('l2_leaf_reg', 0.0001, 10, log=True),
-            'random_strength': lambda trial: trial.suggest_float('random_strength', 0.1, 10),
-            'bagging_temperature': lambda trial: trial.suggest_float('bagging_temperature', 0, 2),
-            'max_ctr_complexity': lambda trial: trial.suggest_int('max_ctr_complexity', 1, 4),
-            'early_stopping_rounds': 25,
-            'verbose': False
-        }
-        if parameter_ranges is not None:
-            self.parameter_ranges.update(parameter_ranges)
-    
-    def __define_cross_val_score_kws(self, cross_val_score_kws: Optional[Dict] = None) -> NoReturn:
-        self.cross_val_score_kws = {
-            'loss': SimpleTargetLoss(),
-            'additional_metrics': {'mae': MAE(), 'max_ae': MaxAE()},
-        }
-        if cross_val_score_kws is not None:
-            self.cross_val_score_kws.update(cross_val_score_kws)
+    return CrossValidationResult(
+        metrics=cv_loss,
+        mean_metrics={key: np.mean(vals) for key, vals in cv_loss.items()},
+        models=models
+    )
+
+def split_period_for_cross_val(
+    min_date: pd.Timestamp,
+    max_date: pd.Timestamp,
+    test_size_weeks: int = 1,
+    train_size_weeks: int = 4,
+    n_folds: int = 5,
+    seed: int = 42
+) -> List[Tuple[List[int], List[int]]]:
+    days = pd.date_range(min_date, max_date, freq='1D', inclusive='both')
+    random.seed(seed)
+    # Defining borders for weeks 
+    # (if border is near to min/max -> ensures that week is full)
+    min_date_week_start = (
+        min_date.to_period('W').start_time 
+        + pd.DateOffset(days=1) # to Mondays
+    )
+    if (min_date_week_start - min_date).days < 0:
+        min_date_week_start += pd.DateOffset(weeks=1)
+    max_date_week_start = (
+        max_date.to_period('W').start_time 
+        + pd.DateOffset(days=1) # to Mondays
+    )
+    if (max_date - max_date_week_start).days < 0:
+        max_date_week_start -= pd.DateOffset(weeks=1)
+
+    # Defining borders for week starts which can be chosen as separator of train and test
+    # (for min date must be enough train data and for max date must be enough test data)
+    min_possible_date = min_date_week_start + pd.DateOffset(weeks=train_size_weeks)
+    max_possible_date = max_date_week_start - pd.DateOffset(weeks=test_size_weeks)
+    # all possible week starts
+    week_starts = pd.date_range(
+        min_possible_date, max_possible_date, freq='1W', inclusive='both'
+    ) + pd.DateOffset(days=1) # to Mondays
+    # randomly choose n_folds from all week_starts
+    if n_folds > len(week_starts):
+        raise ValueError('n_folds must be less or equal than number of suitable weeks presented between min_date and max_date')
+    chosen_weeks = random.sample(sorted(week_starts), n_folds)
+    idx_splits = []
+    for week_start in chosen_weeks:
+        week_start_idx = np.where(days >= week_start)[0][0]
+        train_idx = np.arange(week_start_idx - train_size_weeks*7, week_start_idx)
+        test_idx = np.arange(week_start_idx, week_start_idx + test_size_weeks*7)
+        idx_splits.append((train_idx, test_idx))
+    return idx_splits
