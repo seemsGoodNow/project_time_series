63602622cb3f5432d04140660add7800419ec712
Vsevolod Shurygin <79019835+UralTime@users.noreply.github.com>
2025-04-23 16:30:12 +0300

    external feature engineering strategies from tkalich branch

--- src/feature_engineering/strategies.py
+++ src/feature_engineering/strategies.py
@@ -10,7 +10,8 @@ import pandas as pd
 
 from .engineers import (
     TimeSeriesFeatureEngineer,
-    TaxFeatureEngineer
+    TaxFeatureEngineer,
+    ExternalFactorsFeatureEngineer
 )
 
 
@@ -27,6 +28,7 @@ class BaselineFeatureEngineerStrategy(BaseFeatureEngineerStrategy):
         date_col: str = 'date',
         lags: Optional[Iterable[int]] = None,
     ):
+        self.target = target
         self.time_series_fe = TimeSeriesFeatureEngineer(target=target, date_col=date_col, lags=lags)
         self.tax_fe = TaxFeatureEngineer(target=target, date_col=date_col)
     
@@ -38,4 +40,47 @@ class BaselineFeatureEngineerStrategy(BaseFeatureEngineerStrategy):
     ) -> pd.DataFrame:
         out = self.time_series_fe.build_features(df=df, holidays=holidays)
         out = self.tax_fe.build_features(df=out, taxes=taxes)
+        # Зануляем таргет в праздники и выходные
+        out.loc[(out['is_holiday'] == 1) | (out['is_nowork'] == 1), self.target] = 0
         return out
+
+
+class ExternalFactorsFeatureEngineerStrategy(BaseFeatureEngineerStrategy):
+    def __init__(
+        self,
+        target: str = 'balance',
+        date_col: str = 'date',
+        lags: Optional[Iterable[int]] = None,
+        inflation_df: Optional[pd.DataFrame] = None,
+        currency_df: Optional[pd.DataFrame] = None,
+        moex_df: Optional[pd.DataFrame] = None,
+    ):
+        self.target = target
+        self.date_col = date_col
+        self.time_series_fe = TimeSeriesFeatureEngineer(target=target, date_col=date_col, lags=lags)
+        self.tax_fe = TaxFeatureEngineer(target=target, date_col=date_col)
+
+        self.external_fe = ExternalFactorsFeatureEngineer(
+            inflation_df=inflation_df,
+            currency_df=currency_df,
+            moex_df=moex_df,
+            date_col=date_col
+        )
+
+    def build_features(
+        self,
+        df: pd.DataFrame,
+        taxes: pd.DataFrame,
+        holidays: Dict[str, Sequence[pd.Timestamp]],
+        **kwargs
+    ) -> pd.DataFrame:
+        # Временные признаки на основе таргета
+        out = self.time_series_fe.build_features(df=df, holidays=holidays)
+        out = self.tax_fe.build_features(df=out, taxes=taxes)
+
+        # Признаки внешней среды (инфляция, курс, индекс)
+        out = self.external_fe.build_features(out)
+
+        # Проставляем 0 в таргете на праздники и нерабочие дни
+        out.loc[(out['is_holiday'] == 1) | (out['is_nowork'] == 1), self.target] = 0
+        return out.dropna()
