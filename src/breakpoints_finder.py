49978baf0be1ea621d18aca5f5c81816b1a40ce6
Vsevolod Shurygin <79019835+UralTime@users.noreply.github.com>
2025-04-27 10:51:33 +0000

    breakpoints by SR and CumSum

--- src/breakpoints_finder.py
+++ src/breakpoints_finder.py
@@ -3,8 +3,7 @@ from scipy.stats import norm
 
 class BreakpointFinder():
     
-    def __init__(self, alpha = 0.05, beta = 0.005, diff = -0.01, ceil = 1e6, trsh=0.03, breaks_max=3,
-                slice_length=7):
+    def __init__(self, alpha = 0.05, beta = 0.05, mean_diff = 0.01, sigma_diff = 1, ceil = 1e6, mean_trsh=0.03, sigma_trsh=2, breaks_max=5, slice_length=7):
         
         self.mean_hat = 0
         self.std_hat = 1
@@ -12,14 +11,21 @@ class BreakpointFinder():
         self.alpha = alpha
         self.beta = beta
         
-        self.metric = 0
+        self.SRMetric = 0
+        self.CumSumMetric = 0
+
+        # ГИПЕРПАРАМЕТР: максимальная допустимая разность средних
+        self.mean_diff = mean_diff 
+        # ГИПЕРПАРАМЕТР: максимальная допустимая разность стандратного отклонения
+        self.sigma_diff = sigma_diff 
         
-        # ГИПЕРПАРАМЕТР: дельта для альтернативы
-        self.diff = diff 
-         # ГИПЕРПАРАМЕТР: верхняя граница для значения критерия
-        self.ceil = ceil
+        # ГИПЕРПАРАМЕТР: дельта для альтернативный гипотезы
+        self.mean_diff = mean_diff 
         # ГИПЕРПАРАМЕТР: порог для критерия
-        self.trsh = trsh
+        self.mean_trsh = mean_trsh
+        self.sigma_trsh = sigma_trsh
+        # ГИПЕРПАРАМЕТР: верхняя граница для значения критерия
+        self.ceil = ceil
         # ГИПЕРПАРАМЕТРЫ: макс. число разладок для принятия мер
         self.breaks_max = breaks_max
         
@@ -27,7 +33,7 @@ class BreakpointFinder():
         self.breakpoints = []
         # ГИПЕРПАРАМЕТР: глубина среза значений метрики
         self.slice_length = slice_length
-        self.colors=['red', 'green']
+        self.colors=['green', 'red']
         
     def get_values(self):
         # оцениваем mean и std^2
@@ -41,13 +47,13 @@ class BreakpointFinder():
     def update(self, new_value):
 
         self.get_values()
+        
+        # нормализация и стандартизация
+        self.new_value_normalized = (new_value - self.mean_hat) / np.sqrt(self.std_hat)
 
         # Считаем обновлённое значение std и среднее
         self.predicted_diff_value = (new_value - self.mean_hat) ** 2
         self.predicted_diff_mean = self.var_hat
-        
-        # нормализация и стандартизация
-        self.new_value_normalized = (new_value - self.mean_hat) / np.sqrt(self.std_hat)
         
         # обновляем значения mean и std^2
         try:
@@ -68,18 +74,17 @@ class BreakpointFinder():
             self.var_weights_sum = 1.0      
 
     def count_metric(self):
-
         # проверка гипотезы о том, что среднее действительно = 0, с учётом того, что std = 1
         zeta_k = np.log(norm.pdf(self.new_value_normalized, self.mean_diff, 1) / 
                   norm.pdf(self.new_value_normalized, 0, 1))
-        self.CumSumMetric = max(0, self.metric + zeta_k)
+        self.CumSumMetric = max(0, self.CumSumMetric + zeta_k)
 
         # проверка гипотезы о том, что среднее у разницы между std действительно = 0
         adjusted_value = self.predicted_diff_value - self.predicted_diff_mean
         likelihood = np.exp(self.sigma_diff * (adjusted_value - self.sigma_diff / 2.))
-        self.SRmetric = min(self.ceil, (1. + self.metric) * likelihood)
+        self.SRMetric = min(self.ceil, (1. + self.SRMetric) * likelihood)
 
-        if self.CumSumMetric > self.trsh or self.SRmetric > self.trsh:            
+        if self.CumSumMetric > self.mean_trsh or self.SRMetric > self.sigma_trsh:            
             self.states.append(1)
         else:
             self.states.append(0)
